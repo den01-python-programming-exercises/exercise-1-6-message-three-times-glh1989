877fdc0c7efb38a6313aca20ea8337a66fc6abfe
gh1989 <hughesg368@hwbcymru.net>
2021-01-26 21:47:51 +0000

    Attempt solution 1.6

--- src/exercise.py
+++ src/exercise.py
@@ -3,7 +3,7 @@ def main():
     message = input("Write a message...")
     print(message)
     print(message)
-    print(message)
-    
+    print(message) 
+
 if __name__ == '__main__':
     main()
